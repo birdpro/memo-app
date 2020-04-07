1d981f92225689208cc582d5239e58c375996d9e
EC2 Default User <ec2-user@ip-172-31-82-240.ec2.internal>
2020-04-07 18:14:47 +0000

    add 削除と編集機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get "/", to: "memos#index"
   get "/new", to: "memos#new"
   post "/create",to: "memos#create"
-  
+  delete "/memos/:id", to: "memos#destroy"
+  get "memos/:id/edit", to: "memos#edit"
+  patch "/memos/:id", to: "memos#update"
 end
