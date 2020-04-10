418a38c60760bad45b6aeeff45a69156ed6e8d72
EC2 Default User <ec2-user@ip-172-31-82-240.ec2.internal>
2020-04-10 21:04:04 +0000

    secound commit

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,7 @@
+class CategoriesController < ApplicationController
+    def show
+        @memos = Memo.where(category_id: params[:id])
+        @category = Category.find(params[:id])
+        
+    end
+end
